26be788c4decb811b4215b57e3c089808e5e6996
drewburton <drew.burton.gc@gmail.com>
2023-12-04 18:14:24 -0500

    finished day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,5 +1,6 @@
 import fileinput
 import re
+import functools
 
 def map_to_int(arr) :
 	to_remove = []
@@ -12,7 +13,7 @@ def map_to_int(arr) :
 		arr[i] = int(arr[i])
 	return [i for i in arr if i not in to_remove]
 
-total = 0
+cards = []
 for line in fileinput.input(files="input.txt") :
 	halves = line.split(' | ')
 	parts = halves[0].split(' ')
@@ -20,9 +21,15 @@ for line in fileinput.input(files="input.txt") :
 		parts.remove(parts[0])
 	winning = set(map_to_int(parts))
 	nums = map_to_int(halves[1].split(' '))
-	points = 0
+	matching = 0
 	for num in nums :
 		if num in winning :
-			points = 1 if points == 0 else 2 * points
-	total += points
-print(total)
+			matching += 1
+	cards.append([1, matching])
+
+for i in range(len(cards)) :
+	start = i + 1
+	end = i + 1 + cards[i][1]
+	for j in range(start, end) :
+		cards[j] = [cards[j][0] + cards[i][0], cards[j][1]]
+print(functools.reduce(lambda a, b : [a[0] + b[0], 0], cards)[0])
