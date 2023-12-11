6910a65841dc72b35b122e449f6856e7ca4f9ece
drewburton <drew.burton.gc@gmail.com>
2023-12-11 15:49:47 -0500

    finished day 10

--- day10/day10.py
+++ day10/day10.py
@@ -1,6 +1,7 @@
 import fileinput
 import math
 
+# initilaize map and solution grid
 tiles = []
 start = []
 for line in fileinput.input(files='input.txt') :
@@ -8,15 +9,16 @@ for line in fileinput.input(files='input.txt') :
 		start = [len(tiles), line.find('S')]	
 	tiles.append([x for x in line.strip()])	
 
-solution = []
+pipe = []
 for i in range(len(tiles)) :
 	list = []
 	for j in range(len(tiles[0])) :
-		list.append(float('inf'))
-	solution.append(list)
+		list.append('.')
+	pipe.append(list)
 
-solution[start[0]][start[1]] = 0
+pipe[start[0]][start[1]] = 'S'
 
+# fill pipe into solution grid
 def connects(i1, j1, i2, j2) :
 	# if statement checks whether current pipe location connects to adjacent
 	# return statement checks whether adjacent pipe connects to current
@@ -32,30 +34,101 @@ def connects(i1, j1, i2, j2) :
 
 def take_step(i1, j1, i2, j2) :
 	if connects(i1, j1, i2, j2) :
-		solution[i2][j2] = min(solution[i2][j2], solution[i1][j1] + 1)
-		stack.append((i2, j2, i1, j1))
+		pipe[i2][j2] = tiles[i2][j2]
+		stack.append((i2, j2))
 
 def steps() :
-	stack.append((start[0], start[1], start[0], start[1]))
+	stack.append((start[0], start[1]))
 	while len(stack) > 0 :
-		i, j, si, sj = stack.pop()
-		if i > 0 and si != i - 1:
+		i, j = stack.pop()
+		if i > 0 and pipe[i - 1][j] == '.' :
 			take_step(i, j, i - 1, j)
-		if i < len(tiles) - 1 and si != i + 1:
+		if i < len(tiles) - 1 and pipe[i + 1][j] == '.':
 			take_step(i, j, i + 1, j)
-		if j > 0 and sj != j - 1:
+		if j > 0 and pipe[i][j - 1] == '.':
 			take_step(i, j, i, j - 1)
-		if j < len(tiles[i]) - 1 and sj != j + 1:
+		if j < len(tiles[i]) - 1 and pipe[i][j + 1] == '.':
 			take_step(i, j, i, j + 1)
 
 stack = []
 steps()
 
-max = 0
-for line in solution :
-	for val in line :
-		#print(str(val) + " ", end='')
-		if not math.isinf(val) :
-			max = val if val > max else max
-	#print()
-print(max)
+# expand the grid
+expanded = []
+for i in range(len(pipe) * 2) :
+	list = []
+	for j in range(len(pipe[0]) * 2) :
+		if i % 2 == 1 and j % 2 == 1 :
+			list.append(pipe[int(i / 2)][int(j / 2)])
+		else :
+			list.append('.')
+	expanded.append(list)
+
+# connect
+
+updates = {}
+for i in range(len(expanded)) :
+	for j in range(len(expanded[i])) :
+		if expanded[i][j] == '|' :
+			updates[(i - 1, j)] = '|'
+			updates[(i + 1, j)] = '|'
+		elif expanded[i][j] == '-' :
+			updates[(i, j + 1)] = '-'
+			updates[(i, j - 1)] = '-'
+		elif expanded[i][j] == 'L' :
+			updates[(i - 1, j)] = '|'
+			updates[(i, j + 1)] = '-'
+		elif expanded[i][j] == 'J' :
+			updates[(i - 1, j)] = '|'
+			updates[(i, j - 1)] = '-'
+		elif expanded[i][j] == '7' :
+			updates[(i + 1, j)] = '|'
+			updates[(i, j - 1)] = '-'
+		elif expanded[i][j] == 'F' :
+			updates[(i + 1, j)] = '|'
+			updates[(i, j + 1)] = '-'
+for key in updates :
+	i, j = key
+	expanded[i][j] = updates[key]
+
+# find outside
+outside = set()
+for i in range(len(expanded)) :
+	outside.add((i, 0))
+	outside.add((i, len(expanded[i]) - 1))
+for j in range(len(expanded[0])) :
+	outside.add((0, j))
+	outside.add((len(expanded) - 1, j))
+
+stack = []
+for element in outside :
+	stack.append(element)
+
+while len(stack) > 0 :
+	i, j = stack.pop()
+	if expanded[i][j] != '.' :
+		continue
+
+	expanded[i][j] = 'O'
+	if i > 0 and expanded[i - 1][j] == '.':
+		stack.append((i - 1, j))
+	if i < len(expanded) - 1 and expanded[i + 1][j] == '.':
+		stack.append((i + 1, j))
+	if j > 0 and expanded[i][j - 1] == '.':
+		stack.append((i, j - 1))
+	if j < len(expanded[i]) - 1 and expanded[i][j + 1] == '.':
+		stack.append((i, j + 1))
+
+count = 0
+for i in range(len(expanded)) :
+	for j in range(len(expanded[i])) :
+		if expanded[i][j] == '.' and pipe[int(i / 2)][int(j / 2)] == '.' :
+			pipe[int(i / 2)][int(j / 2)] = 'I'
+			count += 1
+print(count)
+
+# for line in pipe :
+# 	for val in line :
+# 		print(str(val) + " ", end='')
+# 	print()
+
